61fc30c8374abab2e474d079d0ab9a5a9b7fabd8
David Herdlicka <d.herdlicka@firestart.com>
2021-01-26 09:55:16 +0100

    feat: logarithmic brush size

--- src/app/util/caria.util.ts
+++ src/app/util/caria.util.ts
@@ -3,3 +3,12 @@ import {SLIDER_COUNT, VALUE_RANGE} from '../services/caria-service/caria.config'
 export function randomValues(): number[] {
   return Array.from({length: SLIDER_COUNT}, () => Math.random() * VALUE_RANGE * 2 - VALUE_RANGE);
 }
+
+export function convertToActualSize(size: number) {
+  size += 0.3;
+  return 30 * Math.pow(size, 3);
+}
+
+export function convertFromActualSize(size: number) {
+  return Math.pow(size, 1 / 3) / 30 - 0.3;
+}
