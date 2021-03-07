38f10f83292c131cbbd4290f4729be667aa8c202
Henrik Gruber <henrik.gruber2@gmail.com>
2021-03-07 01:08:35 +0100

    - Added small fixes to support high resolution images  - updated slider count

--- src/app/services/car-service/car.config.ts
+++ src/app/services/car-service/car.config.ts
@@ -1,3 +1,5 @@
-export const SLIDER_COUNT = 100;
+export const SLIDER_COUNT = 64;
 export const VALUE_RANGE = 2.5;
+export const IMAGE_HEIGHT = 256;
+export const IMAGE_WIDTH = 768;
 export const BASE_URL = 'http://localhost:8000/';
