7cddb1ed2ba0fe55735dad6525aace4863e33da9
David Herdlicka <d.herdlicka@firestart.com>
2021-04-11 20:26:04 +0200

    feat

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(forceSSL());
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/caria2ui/index.html'));
 });
 // Run the app by serving the static files
 // in the dist directory
